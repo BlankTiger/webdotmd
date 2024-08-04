1608df2a5d567dac57e078a221927218ea139067
Maciej Urban <maciej-urban@outlook.com>
2024-08-04 20:26:52 +0200

    add documentation for webdotx

--- webdotmd/src/main.rs
+++ webdotmd/src/main.rs
@@ -5,8 +5,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let args = parse_args();
     let templates_path = std::path::Path::new("templates");
     let pages_path = std::path::Path::new("pages");
-    let output_path = std::path::Path::new("output");
-    let templates = load_templates(templates_path)?;
+    // let output_path = std::path::Path::new("output");
+    let templates = load_templates(templates_path, Some("html"))?;
     let md_pages = load_markdown_pages(pages_path)?;
     let html_pages = render(&md_pages, &templates);
     dbg!(&templates);
--- webdotx/src/template.rs
+++ webdotx/src/template.rs
@@ -1,13 +1,64 @@
 use crate::utils::load_files_in_dir_to_string;
 use std::{collections::HashMap, path::Path};
 
+/// Fundamental data structure of webdotx, holds template content and parsed placeholders.
+/// Placeholder always starts with `{{ $`, and ends with `$ }}`, name of the placeholder is between
+/// `$`. Example template:
+///
+/// ```html
+/// <html>
+///
+/// <head>
+///     <title>{{ $title$ }}</title>
+/// </head>
+///
+/// <body>
+///     <h1>{{ $header$ }}</h1>
+///     {{ $content$ }}
+/// </body>
+///
+/// </html>
+/// ```
+///
+/// For that template, `Template` struct internally would look like this:
+/// ```rust ignore
+/// let template = Template {
+///     content: "<html>\n\n<head>\n    <title>{{ $title$ }}</title>\n</head>\n\n<body>\n    <h1>{{ $header$ }}</h1>\n    {{ $content$ }}\n</body>\n\n</html>\n".to_string(),
+///     placeholders: [
+///         Placeholder {
+///             name: "title",
+///             position: Position {
+///                 from: 26,
+///                 to: 38,
+///             },
+///         },
+///         Placeholder {
+///             name: "header",
+///             position: Position {
+///                 from: 33,
+///                 to: 46,
+///             },
+///         },
+///         Placeholder {
+///             name: "content",
+///             position: Position {
+///                 from: 10,
+///                 to: 24,
+///             },
+///         },
+///     ],
+/// };
+/// ```
+///
+/// Notice that `position.from` is the index of the first character of the placeholder counting from
+/// the beginning of the template content string if it's the first placeholder, or from the end of
+/// the previous placeholder if it's not the first one.
 #[derive(Debug)]
 pub struct Template {
     content: String,
     placeholders: Vec<Placeholder>,
 }
 
-/// Placeholder always starts with '{{ $', and ends with '$ }}'.
 #[derive(Debug, PartialEq)]
 struct Placeholder {
     /// Name is between '$'.
@@ -26,8 +77,25 @@ struct Position {
     to: usize,
 }
 
-pub fn load_templates(templates_path: &Path) -> Result<HashMap<String, Template>, std::io::Error> {
-    let template_strings = load_files_in_dir_to_string(templates_path, Some("html"))?;
+/// Loads templates from the provided directory with provided extension. If extension is `None`, then
+/// all files in the directory are loaded.
+///
+/// Returns a map of template names (paths) to `Template` structs.
+///
+/// Example use:
+/// ```rust
+/// use std::collections::HashMap;
+/// use webdotx::{load_templates, Template};
+///
+/// let templates_path = std::path::Path::new("templates");
+/// let templates = load_templates(templates_path, Some("html"))/* .unwrap() */;
+/// // let template = templates.get("template.html").unwrap();
+/// ```
+pub fn load_templates(
+    templates_path: &Path,
+    extension: Option<&str>,
+) -> Result<HashMap<String, Template>, std::io::Error> {
+    let template_strings = load_files_in_dir_to_string(templates_path, extension)?;
     let mut templates = HashMap::new();
     for (template_path, template_content) in template_strings {
         let mut template = Template {
@@ -65,6 +133,8 @@ fn parse_placeholders(template_content: &str) -> Vec<Placeholder> {
 }
 
 impl Template {
+    /// Fills placeholders in the template with the provided values. If value for a placeholder is
+    /// missing, then it returns an error.
     pub fn fill_template(&self, filled_placeholders: HashMap<String, String>) -> String {
         let mut rendered = String::new();
         let mut content = self.content.as_str();
@@ -79,10 +149,91 @@ impl Template {
     }
 }
 
+/// Renderable allows for rendering of data structures into a string using provided templates.
+///
+/// Example use:
+///
+/// ```rust
+/// use std::collections::HashMap;
+/// use webdotx::{Renderable, Template};
+///
+/// struct Page {
+///    name: String,
+///    content: String,
+///    test_offset: String,
+///    template_name: String,
+///    // other fields
+/// }
+///
+/// impl Renderable for Page {
+///    fn render(&self, templates: &HashMap<String, Template>) -> String {
+///        let filled_placeholders = HashMap::from([
+///            ("name".to_string(), self.name.clone()),
+///            ("content".to_string(), self.content.clone()),
+///            ("test_offset".to_string(), self.test_offset.clone()),
+///        ]);
+///        let template = templates.get(&self.template_name).unwrap();
+///        template.fill_template(filled_placeholders)
+///    }
+/// }
+/// ```
 pub trait Renderable {
     fn render(&self, templates: &HashMap<String, Template>) -> String;
 }
 
+/// Renders all renderables using provided templates. Returns a map of renderable names to rendered
+/// strings.
+///
+/// Example use:
+/// ```rust ignore
+/// use std::collections::HashMap;
+/// use webdotx::{render, Renderable, Template};
+///
+/// struct Page {
+///   name: String,
+///   content: String,
+///   test_offset: String,
+///   template_name: String,
+///   // other fields
+/// }
+///
+/// impl Renderable for Page {
+///     fn render(&self, templates: &HashMap<String, Template>) -> String {
+///         let filled_placeholders = HashMap::from([
+///             ("name".to_string(), self.name.clone()),
+///             ("content".to_string(), self.content.clone()),
+///             ("test_offset".to_string(), self.test_offset.clone()),
+///             // other fields
+///         ]);
+///         let template = templates.get(&self.template_name).unwrap();
+///         template.fill_template(filled_placeholders)
+///     }
+/// }
+///
+/// let named_renderables = HashMap::from([
+///     (
+///         "page1".to_string(),
+///         Page {
+///             name: "page1".to_string(),
+///             content: "some content".to_string(),
+///             test_offset: "tested".to_string(),
+///             template_name: "template1".to_string(),
+///         },
+///     ),
+///     (
+///         "page2".to_string(),
+///         Page {
+///             name: "page2".to_string(),
+///             content: "some other content".to_string(),
+///             test_offset: "tested".to_string(),
+///             template_name: "template2".to_string(),
+///         },
+///     ),
+/// ]);
+///
+/// let templates = load_templates(std::path::Path::new("templates")).unwrap();
+/// let rendered = render(&named_renderables, &templates);
+/// ```
 pub fn render(
     named_renderables: &HashMap<String, impl Renderable>,
     templates: &HashMap<String, Template>,
